8413a3f3049a2b94050a26f266a9cf9197362438
AmirHosein Gholamrezaee <amirhosein80.gholamrezaee@gmail.com>
2023-06-28 11:42:58 +0330

    impelement open digikala

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as chromeService
-
 driver = webdriver.Chrome(service=chromeService(ChromeDriverManager().install()))
+driver.get('https://digikala.com')
